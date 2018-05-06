19033f7a99563db8af323680872866b92790ec2c
donbattery <mikloslorinczi.it@gmail.com>
2018-05-06 19:40:22 +0200

    dependencyes fixed

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+// const cors = require('cors');
 const path = require('path');
 const logger = require('endpointz').reqLog;
 const serverLog = require('endpointz').serverLog;
 const startMessage = require('endpointz').startMessage;
-const handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
 const favicon = require('serve-favicon');
 
 const PORT = (process.argv[2] || 8080);
@@ -17,29 +17,13 @@ const serverName = (process.argv[3] || 'ribbit');
 
 app.use(logger);
 
+// app.use(cors());
+
 app.use(express.json());
 
 app.use(favicon(path.join(__dirname,'public','img','favicon.ico')));
 
-app.engine('handlebars', handlebars.engine);
-
-app.set('view engine', 'handlebars');
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function(req, res) {
-
-  res.render('home');
-});
-
-app.get('/about', function(req, res) {
-
-  res.render('about');
-});
-
-app.get('/chat', function(req, res) {
-  res.render('chat');
-});
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
@@ -52,6 +36,7 @@ app.use(function(err, req, res, next){
   res.status(500).render('500');
 });
 
+// Socket.IO 
 io.on('connection', function(socket){
   serverLog('User connected with the ID : ' + socket.id);
   socket.on('login', nickName => {
@@ -59,4 +44,5 @@ io.on('connection', function(socket){
   });
 });
 
+// Start main loop
 http.listen(PORT, startMessage(serverName, PORT));
